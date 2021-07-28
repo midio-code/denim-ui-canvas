76cb73698d64179ee6c7ed8a92801e927d4e0630
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2021-07-29 01:29:26 +0200

    Lerping colors on performance visualization diagram

--- src/performance.nim
+++ src/performance.nim
@@ -6,6 +6,9 @@ import canvas
 import math
 import colors
 
+proc lerp(t, a, b: float): float =
+  return a + t * (b - a);
+
 var performance {.importc, nodecl.}: dom.Performance
 
 type
@@ -66,8 +69,13 @@ proc drawLastFrame(self: Performance): void =
   let lastFrameTime = lastFrame.endTime - lastFrame.startTime
   let barHeight = (lastFrameTime / (16.0 * 2.0)) * height
 
+  let goodness = clamp(lastFrameTime / 16.0, 0.0, 1.0)
+  let red = goodness.lerp(0.0, 255.0).clamp(0, 255)
+  let green = goodness.lerp(255.0, 0.0).clamp(0, 255)
+  let color = rgb(red.int, green.int, 0)
+
   performanceCanvasContext.clearRect(barWidth * lastFrameIndex.float, 0.0, barWidth, height)
-  performanceCanvasContext.fillStyle = "#ff0000"
+  performanceCanvasContext.fillStyle = $color
   performanceCanvasContext.fillRect(barWidth * lastFrameIndex.float, height - barHeight, barWidth, barHeight)
   performanceCanvasContext.fillStyle = "#004400"
   performanceCanvasContext.fillRect(0.0, height / 2.0, width, 1.0)
